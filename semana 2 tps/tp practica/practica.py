f4437e4cfe9df465f5a78136186305b60d248f4a
FortunatoSebastian <sebastianfortunato22@gmail.com>
2026-02-18 10:22:25 -0300

    carpeta de practica

---
+++ semana 2 tps/tp practica/practica.py
@@ -0,0 +1,23 @@
+#1
+# numero = int(input("Ingresar numero para saber si es par o impar: "))
+
+# if numero % 2 == 0:
+#     print("Es numero par")
+# else:
+#     print("Es numero impar")
+
+#2
+# nombre_usuario = input("Ingresar Nombre de Usuario: ")
+# contraseña = int(input("Ingresar Contraseña: "))
+
+# permitir_usuario = "pepe"
+# permitir_contraseña = 1234
+
+# if nombre_usuario == permitir_usuario and contraseña == permitir_contraseña:
+#     print("Acceso permitido")
+# else:
+#     print("Acceso denegado")
+
+#3
+
+
