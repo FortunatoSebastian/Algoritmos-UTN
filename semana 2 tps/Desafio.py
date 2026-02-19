12b578825dc38a84d48da0adbc0f16afed1ec519
FortunatoSebastian <sebastianfortunato22@gmail.com>
2026-02-19 10:07:14 -0300

    fibonacci

--- semana 2 tps/Desafio.py
+++ semana 2 tps/Desafio.py
@@ -23,5 +23,23 @@
 # contador = contador +1
 # print(contador)
 
-for i in range(1, 11):
-    print(i)
+# for i in range(1, 11):
+#     print(i)
+#while for vectores = listas 
+# fibonacci 
+import os
+os.system("cls")
+
+a = 0
+b = 1
+lista = []
+for i in range(32):
+
+    a,b = (b, a+b)
+    if a % 2 == 0:
+        lista.append(a)
+        print(a)
+        
+    #   print("El valor actual de a y es PAR:", a)
+    # else:
+    #   print("El valor actual de a y es IMPAR:", a)
