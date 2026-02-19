d91f113fa7a05efd04a8fa2d135f319d020474c1
FortunatoSebastian <sebastianfortunato22@gmail.com>
2026-02-19 14:20:56 -0300

    ejercicios de practica

---
+++ semana 2 tps/Ejercicios de practica/1 ejercicio for.py
@@ -0,0 +1,31 @@
+# import os
+# os.system("cls")
+#1
+# numero = int(input("Que numero quieres multiplicar? "))
+
+# for i in range(1,11):
+#     print(f"{numero} x {i} = {numero * i}")
+
+#2
+# letra = "palabra"
+
+# for i in range(len(letra)):
+#     print(f"{i + 1} - {letra[i]}")
+
+#3
+# lista_numeros = [2,3,5,1,6,88,55,63,64,62,72,7474]
+
+# for n in range(len(lista_numeros)):
+#     if n % 2 == 0:
+#         print(f"{lista_numeros[n]} Es numero PAR")
+
+#4
+# numero = int(input("Ingresa el numero que quieras saber si es primo: "))
+
+
+# if numero % 3 == 0:
+#     print(f"{numero} SI es numero primo")
+# else:
+#     print(f"{numero} No es numero primo")
+
+#5
