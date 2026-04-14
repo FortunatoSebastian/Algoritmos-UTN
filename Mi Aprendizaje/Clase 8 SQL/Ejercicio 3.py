43bf018bf6167e8e4c52a0cb68599f889d83d102
FortunatoSebastian <sebastianfortunato22@gmail.com>
2026-04-14 01:03:02 -0300

    Sistema de Facturacion - Agregado de Inventario

--- Mi Aprendizaje/Clase 8 SQL/Ejercicio 3.py
+++ Mi Aprendizaje/Clase 8 SQL/Ejercicio 3.py
@@ -11,15 +11,44 @@ cursor.execute("""
         precio INTEGER,
         total INTEGER)
 """)
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS inventario(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nombre TEXT,
+        precio INTEGER)
+""")
+
+def agregar_producto():
+    os.system("cls")
+    nombre = input("Ingrese nombre del producto: ")
+    precio = int(input("Ingrese el precio del producto: "))
+
+    cursor.execute("INSERT INTO inventario (nombre, precio) VALUES (?, ?)", (nombre, precio))
+    conexion.commit()
 
 def registrar_venta():
     os.system("cls")
     producto = input("Ingrese el nombre del producto: ")
     cantidad = int(input("Ingrese la cantidad que quiera comprar: "))
-    precio = int(input("Ingrese el precio del producto: "))
 
-    cursor.execute("INSERT INTO ventas (producto, cantidad, precio, total) VALUES (?, ?, ?, ?)", (producto, cantidad, precio, cantidad * precio))
+    cursor.execute("SELECT precio FROM inventario WHERE nombre = ?", (producto,))
+    resultado = cursor.fetchone()
+    
+    if resultado is None:
+        print("Erros: El producto no existe en el Inventario")
+        input("\nPresione Enter para volver al menu...")
+        return
+
+    cursor.execute("INSERT INTO ventas (producto, cantidad, precio, total) VALUES (?, ?, ?, ?)", (producto, cantidad, resultado[0], cantidad * resultado[0]))
     conexion.commit()
+
+def ver_inventario():
+    os.system("cls")
+    cursor.execute("SELECT * FROM inventario")
+    filas = cursor.fetchall()
+    for fila in filas:
+        print(fila)
+    input("\nPresione Enter para volver al menú...")
 
 def ver_ventas():
     os.system("cls")
@@ -49,19 +78,25 @@ def menu():
         print("="*30)
         print("   Sistemas de facturación")
         print("="*30)
-        print("1 -Registrar venta")
-        print("2 -Ver ventas")
-        print("3 -Ver total recaudado")
-        print("4 -Salir")
+        print("1 -Registrar Producto")
+        print("2 -Registrar Venta")
+        print("3 -Ver Ventas")
+        print("4 -Ver Inventario")
+        print("5 -Ver Total Recaudado")
+        print("6 -Salir")
         opciones = input("\nIngrese la opcion deseada: ")
         match opciones:
             case "1":
-                registrar_venta()
+                agregar_producto()
             case "2":
-                ver_ventas()
+                registrar_venta()
             case "3":
-                total_recaudado()
+                ver_ventas()
             case "4":
+                ver_inventario()
+            case "5":
+                total_recaudado()
+            case "6":
                 print("Hasta luego!")
                 break
 
